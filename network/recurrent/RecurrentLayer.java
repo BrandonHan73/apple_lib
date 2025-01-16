0925115bbc7d8412c6b3d55e0842d765707b7142
Brandon Han <brandontthan2005@gmail.com>
2025-01-15 18:59:52 -0600

    Recurrent backpropogation

--- network/recurrent/RecurrentLayer.java
+++ network/recurrent/RecurrentLayer.java
@@ -1,6 +1,7 @@
 package apple_lib.network.recurrent;
 
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import apple_lib.network.layer;
@@ -71,6 +72,51 @@ public abstract class RecurrentLayer extends ANN_Layer {
 		y_history.addFirst(last_y);
 
 		return out;
+	}
+
+	@Override
+	protected void load_derivatives(double... dCdy) {
+		if(last_x == null || last_z == null || last_y == null) {
+			throw new RuntimeException("Must feed forward before backpropogating");
+		}
+		if(dCdy.length != output_count) {
+			throw new RuntimeException(String.format("Expected %d inputs, %d provided", output_count, dCdy.length));
+		}
+
+		int history_length = x_history.size();
+		double[][] total_dCdw = new double[input_count][output_count];
+		double[] total_dCdb = new double[output_count];
+		for(int output = 0; output < output_count; output++) {
+			total_dCdb[output] = 0;
+			for(int input = 0; input < input_count; input++) {
+				total_dCdw[input][output] = 0;
+			}
+		}
+
+		Iterator<double[]> x_history_it = x_history.iterator();
+		Iterator<double[]> z_history_it = z_history.iterator();
+		Iterator<double[]> y_history_it = y_history.iterator();
+		for(int time = 0; time < history_length; time++) {
+			last_x = x_history_it.next();
+			last_z = z_history_it.next();
+			last_y = y_history_it.next();
+
+			load_derivatives(dCdy);
+			for(int output = 0; output < output_count; output++) {
+				total_dCdb[output] += loaded_dCdb[output];
+				for(int input = 0; input < input_count; input++) {
+					total_dCdw[input][output] += loaded_dCdw[input][output];
+				}
+			}
+
+			dCdy = new double[recurrent_input_count];
+			for(int input = 0; input < recurrent_input_count; input++) {
+				dCdy[input] = loaded_dCdx[user_input_count + input];
+			}
+		}
+
+		loaded_dCdw = total_dCdw;
+		loaded_dCdb = total_dCdb;
 	}
 
 }

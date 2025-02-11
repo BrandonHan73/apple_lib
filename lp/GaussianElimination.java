ca2d851e20c01048c3d47ca0148d4c4b3ada28b4
Brandon Han <brandontthan2005@gmail.com>
2025-02-10 22:55:31 -0600

    Finished Gaussian elimination

--- lp/GaussianElimination.java
+++ lp/GaussianElimination.java
@@ -46,9 +46,9 @@ public class GaussianElimination {
 	}
 
 	/**
-	 * Reduces loaded matrix to row echilon form.
+	 * Reduces loaded matrix to reduced row echilon form.
 	 */
-	public double[][] ref() {
+	public double[][] rref() {
 		if(matrix == null) {
 			throw new IllegalStateException();
 		}
@@ -60,18 +60,25 @@ public class GaussianElimination {
 				out[row][col] = matrix[row][col];
 			}
 		}
+		int col_max = Math.min(N, M);
+
+		// Store variable for each row
+		int[] row_var = new int[N];
+		for(int row = 0; row < N; row++) {
+			row_var[row] = -1;
+		}
 
 		// Main loop
 		for(int fill = 0; fill < N; fill++) {
-
 			// Find largest coefficient
 			double coeff = 0;
 			int target_row = fill;
 			int target_col = 0;
-			for(int row = 0; row < N; row++) {
-				for(int col = 0; col < M; col++) {
-					if(out[row][col] > coeff) {
-						coeff = Math.abs(out[row][col]);
+			for(int row = fill; row < N; row++) {
+				for(int col = 0; col < col_max; col++) {
+					double poll = Math.abs(out[row][col]);
+					if(poll > coeff) {
+						coeff = poll;
 						target_row = row;
 						target_col = col;
 					}
@@ -85,15 +92,40 @@ public class GaussianElimination {
 			double[] swap = out[fill];
 			out[fill] = out[target_row];
 			out[target_row] = swap;
+			row_var[fill] = target_col;
 
 			// Normalize target row
 			coeff = out[fill][target_col];
 			for(int col = 0; col < M; col++) {
+				out[fill][col] /= coeff;
 			}
 
+			// Zero other rows
+			for(int row = fill + 1; row < N; row++) {
+				double base = out[row][target_col];
+				for(int col = 0; col < M; col++) {
+					out[row][col] -= base * out[fill][col];
+				}
+			}
 		}
 
-		return out;
+		// Second pass
+		double[][] reorder = new double[N][M];
+		for(int row = N - 1; row >= 0; row--) {
+			int target_col = row_var[row];
+			if(target_col == -1) continue;
+
+			for(int target = 0; target < row; target++) {
+				double base = out[target][target_col];
+				for(int col = 0; col < M; col++) {
+					out[target][col] -= base * out[row][col];
+				}
+			}
+
+			reorder[target_col] = out[row];
+		}
+
+		return reorder;
 	}
 
 }
